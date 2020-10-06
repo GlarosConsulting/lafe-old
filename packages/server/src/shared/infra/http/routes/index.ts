54b9665d6c9769cecad205ab7c490b97ce5db127
André 'Dezzy <andrevcv1@gmail.com>
2020-10-06 14:35:04 -0300

    feat: Add stock module

--- packages/server/src/shared/infra/http/routes/index.ts
+++ packages/server/src/shared/infra/http/routes/index.ts
@@ -3,12 +3,14 @@ import { Router } from 'express';
 import billsRouter from '@modules/bills/infra/http/routes/bills.routes';
 import expensesRouter from '@modules/expenses/infra/http/routes/expenses.routes';
 import physicalsComparativesRouter from '@modules/physicals_comparatives/infra/http/routes/physicals_comparatives.routes';
+import stockRouter from '@modules/stock/infra/http/routes/stock.routes';
 
 const routes = Router();
 
 routes.use('/bills', billsRouter);
 routes.use('/physicals-comparatives', physicalsComparativesRouter);
 routes.use('/expenses', expensesRouter);
+routes.use('/stock', stockRouter);
 
 routes.get('/', (_request, response) => {
   return response.json({
